d2919c69f6a2857d4052ba2a8b53905c17a7de4e
Aditya Deshpande <f20212681@pilani.bits-pilani.ac.in>
2023-01-14 15:01:13 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,65 @@
+for(var i=0;i<7;i++)
+{
+    
+    document.querySelectorAll("button")[i].addEventListener("click",function()
+    {
+        var buttonclicked=this.innerHTML;
+        playsound(buttonclicked);
+        buttonAnimation(buttonclicked);
+        
+    });
+}
+
+
+
+document.addEventListener("keypress",function(event){
+playsound(event.key);
+buttonAnimation(event.key);
+
+});
+
+function playsound(key)
+{
+    switch (key) 
+    {
+        case "w":
+            var audio =new Audio("sounds/tom-1.mp3");
+            audio.play();
+            break;
+        case "a":
+            var audio =new Audio("sounds/tom-2.mp3");
+            audio.play();
+            break;
+        case "s":
+         var audio =new Audio("sounds/tom-3.mp3");
+         audio.play();
+         break;
+        case "d":
+            var audio =new Audio("sounds/tom-4.mp3");
+            audio.play();
+            break;
+        case "j":
+            var audio =new Audio("sounds/crash.mp3");
+            audio.play();
+            break;
+        case "k":
+            var audio =new Audio("sounds/kick-bass.mp3");
+            audio.play();
+            break;
+        case "l":
+            var audio =new Audio("sounds/snare.mp3");
+            audio.play();
+            break;
+        default:
+            break;
+    }
+}
+
+function buttonAnimation(currentKey)
+{
+var activeButton=document.querySelector("."+currentKey);
+activeButton.classList.add("pressed");
+setTimeout(function() {
+    activeButton.classList.remove("pressed");
+}, 100);
+}
